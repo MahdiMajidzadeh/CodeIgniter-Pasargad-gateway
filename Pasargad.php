68d430b6ed41214710523aebb9b8a4a33b7a027b
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2019-01-06 21:19:35 +0000

    Apply fixes from StyleCI

--- Pasargad.php
+++ Pasargad.php
@@ -24,12 +24,11 @@ class Zarinpal
     private $timestamp;
     private $invoice_date;
     private $data;
-   
 
-    public function request($merchant, $terminal, $amount, $call_back, $invoice_number, $timestamp )
+    public function request($merchant, $terminal, $amount, $call_back, $invoice_number, $timestamp)
     {
-        require_once("RSAProcessor.class.php"); 
-        $processor = new RSAProcessor("certificate.xml",RSAKeyType::XMLFile);
+        require_once 'RSAProcessor.class.php';
+        $processor = new RSAProcessor('certificate.xml', RSAKeyType::XMLFile);
 
         $this->merchant = $merchant;
         $this->terminal = $terminal;
@@ -40,16 +39,15 @@ class Zarinpal
         $this->timestamp = $timestamp;
         $this->invoice_date = $timestamp;
 
-        $data = "#". $merchant ."#". $terminal ."#". $invoice_number ."#". $timestamp ."#". $amount ."#". $call_back ."#". $action ."#". $timestamp ."#";
-        $data = sha1($data,true);
-        $data =  $processor->sign($data);
-        $this->data =  base64_encode($data);
-
+        $data = '#'.$merchant.'#'.$terminal.'#'.$invoice_number.'#'.$timestamp.'#'.$amount.'#'.$call_back.'#'.$action.'#'.$timestamp.'#';
+        $data = sha1($data, true);
+        $data = $processor->sign($data);
+        $this->data = base64_encode($data);
     }
 
     public function redirect_form()
     {
-       return <<<EOT
+        return <<<EOT
 <!DOCTYPE>
 <html>
   <head>
@@ -75,6 +73,5 @@ window.onload = function(){
 </html>
 
 EOT;
-        
     }
 }
